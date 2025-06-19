863da88139947731dd9927a47a3708d4c237a05e
HemanshuMandhana <hemanshumandhana@gmail.com>
2025-06-19 14:51:45 +0530

    Add files via upload

---
+++ src/components/common/Footer1.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-full px-4 sm:px-8 lg:px-20 xl:px-36 pb-4 text-center">
+      <p className="text-[1.6vh] sm:text-[1.8vh] lg:text-[2vh] font-normal text-black font-gilroy">
+        2025, Designed and Developed by Feedback Solutions Inc
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
